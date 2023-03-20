6d762320f4d2f1124f967233e82f66397c612cfc
kenichi beveridge <kenbev84@gmail.com>
2023-03-20 15:08:58 +0000

    Add instance fields name & batteryLevel, Droid constructor and main method

--- src/Droid.java
+++ src/Droid.java
@@ -1,2 +1,12 @@
 public class Droid {
+    String name;
+    int batteryLevel;
+
+    public Droid(String droidName) {
+        name = droidName;
+        batteryLevel = 100;
+    }
+    public static void main(String[] args) {
+
+    }
 }
